89424552de7bcaad6bc1971e23f4a83bbb57492c
Kim HyunWoo <stories2@naver.com>
2023-05-28 18:54:03 +0900

    Init express session

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
-const { Strategy } = require("passport-google-oauth20");
+const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 const winston = require("winston");
 const { v4: uuidv4 } = require("uuid");
 const os = require("os");
+const { StatusCodes } = require("http-status-codes");
 const hostname = os.hostname();
+require("dotenv").config();
 
 const apiVer = "v1";
 
 const app = express();
 const port = 3000;
+
+const failureRedirectUrl = "/#/login";
+const successRedirectUrl = "/#/";
+
+const E_NOT_AUTHORIZED = "e_not_authorized";
 
 const logger = winston.createLogger({
   level: "debug",
@@ -37,6 +44,17 @@ function setFeatureIdToHeaderMiddleware(req, next, featureId) {
 }
 
 /**
+ * @param {*} req Should use this after log system's tracking feature already set
+ * @returns {object}
+ */
+function getReqContext(req) {
+  return {
+    featureId: req.headers["X-Feature-ID"],
+    traceId: req.headers["X-Request-ID"],
+  };
+}
+
+/**
  * @summary Set trace id to tracking log messages.
  * @param {req}
  * @param {res}
@@ -49,25 +67,56 @@ app.use((req, res, next) => {
   if (!req.header("X-Request-ID")) req.headers["X-Request-ID"] = uuidv4();
   res.on("finish", () =>
     logger.info({
-      featureId: req.headers["X-Feature-ID"],
-      traceId: req.headers["X-Request-ID"],
+      ...getReqContext(req),
     })
   );
   res.on("error", (err) =>
     logger.error({
-      featureId: req.headers["X-Feature-ID"],
-      traceId: req.headers["X-Request-ID"],
+      ...getReqContext(req),
       err: err.message,
     })
   );
   next();
 });
 
+/**
+ * Load front-end page
+ */
 app.use(express.static("dist"));
+
+/**
+ * Setup session middleware
+ */
+app.use(
+  session({
+    secret: process.env.EXPRESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CLIENT_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      cb(null, profile);
+    }
+  )
+);
 
 app.get(
   `/oauth2/${apiVer}/authorize/callback`,
   (req, res, next) => setFeatureIdToHeaderMiddleware(req, next, "API001"),
+  passport.authenticate(["google"], {
+    failureRedirect: failureRedirectUrl,
+    successRedirect: successRedirectUrl,
+  }),
   (req, res) => {
     res.send("ok");
   }
@@ -76,16 +125,55 @@ app.get(
 app.post(
   `/oauth2/${apiVer}/authorize/callback`,
   (req, res, next) => setFeatureIdToHeaderMiddleware(req, next, "API002"),
+  passport.authenticate(["google"], {
+    failureRedirect: failureRedirectUrl,
+    successRedirect: successRedirectUrl,
+  }),
   (req, res) => {
     res.send("ok");
   }
 );
 
+/**
+ *
+ * @param {*} req check req.user exist or redirect to login page
+ * @param {*} res
+ * @param {*} next
+ * @returns
+ */
+function ensureLoggedIn(req, res, next) {
+  if (req.user) {
+    return next();
+  }
+  logger.warn({
+    ...getReqContext(req),
+    error: E_NOT_AUTHORIZED,
+  });
+  return res.redirect(failureRedirectUrl);
+}
+
 app.get(
   `/oauth2/${apiVer}/myinfo`,
   (req, res, next) => setFeatureIdToHeaderMiddleware(req, next, "API003"),
+  ensureLoggedIn,
   (req, res) => {
     res.send("ok");
+  }
+);
+
+app.route(`/oauth2/${apiVer}/logout`).all(
+  (req, res, next) => setFeatureIdToHeaderMiddleware(req, next, "API004"),
+  ensureLoggedIn,
+  (req, res) => {
+    res.send("ok");
+  }
+);
+
+app.use(
+  (req, res, next) => setFeatureIdToHeaderMiddleware(req, next, "API999"),
+  (req, res, next) => {
+    res.status(StatusCodes.NOT_FOUND);
+    res.send();
   }
 );
 
