a4321059dbdd64dfa729f22507436fddcd509700
Kim HyunWoo <stories2@naver.com>
2023-05-28 17:15:08 +0900

    Install dependency

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
 const app = express();
 const port = 3000;
 
@@ -7,6 +10,14 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static("dist"));
+
+app.get("/oauth2/v1/authorize/callback", (req, res) => {
+  res.send("ok");
+});
+
+app.post("/oauth2/v1/authorize/callback", (req, res) => {
+  res.send("ok");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
