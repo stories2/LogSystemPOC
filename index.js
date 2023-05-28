31cc5d7ceb45d4b0607daf6d3fd495c056d9b0d8
Kim HyunWoo <stories2@naver.com>
2023-05-28 11:03:36 +0900

    init front proj

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  next();
 });
+
+app.use(express.static("dist"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
