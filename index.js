bd180b2756d0ffb7dfd501e461289baa7bbaf1f2
Kim HyunWoo <stories2@naver.com>
2023-05-28 22:30:44 +0900

    Binding port error

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 const apiVer = "v1";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const failureRedirectUrl = "/#/login";
 const successRedirectUrl = "/#/";
